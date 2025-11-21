10d5108510181955615a4896a2dbe8fd139b4200
Jakub Cwiertka <82480381+cwke@users.noreply.github.com>
2025-11-21 15:15:20 +0100

    Initial commit
    Commit iniziale con la struttura base del progetto:
    - Progetto base netbeans con maven e JUnit
    - .gitignore
    - cartella doc

---
+++ src/main/java/it/unisa/diem/softeng/librarymanager/LibraryManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package it.unisa.diem.softeng.librarymanager;
+
+/**
+ *
+ * @author Jakub
+ */
+public class LibraryManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
